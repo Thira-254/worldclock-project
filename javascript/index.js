1df7004798a3acc09ad7c792b72ab84771bee142
Thira-254 <agathathira60@gmail.com>
2025-10-26 15:24:30 +0300

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,28 @@
 function updateTime() {
   // los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
   // nairobi
   let nairobiElement = document.querySelector("#nairobi");
-  let nairobiDateElement = nairobiElement.querySelector(".date");
-  let nairobiTimeElement = nairobiElement.querySelector(".time");
-  let nairobiTime = moment().tz("Africa/Nairobi");
+  if (nairobiElement) {
+    let nairobiDateElement = nairobiElement.querySelector(".date");
+    let nairobiTimeElement = nairobiElement.querySelector(".time");
+    let nairobiTime = moment().tz("Africa/Nairobi");
 
-  nairobiDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  nairobiTimeElement.innerHTML = nairobiTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    nairobiDateElement.innerHTML = nairobiTime.format("MMMM Do YYYY");
+    nairobiTimeElement.innerHTML = nairobiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
@@ -29,7 +33,7 @@ function updateCity(event) {
   citiesElement.innerHTML = `
   <div class="city">
         <div>
-          <h2>${cityTimeZone}</h2>
+          <h2>${cityName}</h2>
           <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
         <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
